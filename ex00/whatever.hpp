1ef55ab569e7d96061e3c3e7c7b292b19677af4f
aklimchu <aklimchu@student.hive.fi>
2025-02-21 10:09:35 +0200

    ex00 completed

---
+++ ex00/whatever.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template <typename T>
+void swap(T & x, T & y) {
+	T temp = x;
+
+	x = y;
+	y = temp;
+}
+
+template <typename T>
+T max(T const & x, T const & y) {
+	return((x > y) ? x : y);
+}
+
+template <typename T>
+T min(T const & x, T const & y) {
+	return((x < y) ? x : y);
+}
